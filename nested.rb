d7ac109506d556a1fd6cf24c250988c14843cac8
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-10 19:35:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-  programmer_hash[:alan_kay][:known_for]
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  return programmer_hash
 end
 
 def adding_to_dennis
